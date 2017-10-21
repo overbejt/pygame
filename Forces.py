429398ed7d999d4ee7a888caecef36d765c42964
Josh Overbeck <overbejt@miamioh.edu>
2017-10-21 08:42:39 -0400

    fixed OOP

--- Forces.py
+++ Forces.py
@@ -1,9 +1,35 @@
-import numpy as np
 
-gravity = 9.8 #initialized as earth's gravity in m/s^2
+class Forces(object):
+    import numpy as np
 
-def getGravitationalForce(mass):
-    return gravity*mass
 
-def setGravitation(inGravity):
-    gravity = inGravity
+    """This is a class that will handle the forces.  It will have
+        getters and setters.  It will also return the sumation of forces
+
+        Attributes:
+        gravity: A float representing the gravitational force of the planet
+        tension: A float representing the tension in a massless/weightless rope
+
+    """
+
+    def __init__(self):
+        self.gravity = 9.8 #initialized as earth's gravity in m/s^2
+        self.tension = 0.0 #in N
+
+    def getGravitationalForce(self, mass):
+        """Returns the gravitational force on an object"""
+        return self.gravity*mass
+
+    def setGravity(self, inGravity):
+        """Sets the gravitational force of a planet"""
+        self.gravity = inGravity
+
+    def setTensionForce(self, inTension):
+        """Sets the tension force of a massless/weightless rope"""
+        self.tension = inTension
+
+    def getTensionForce(self):
+        """Returns the tension force of a massless/weightless rope"""
+        return self.tension
+
+
--- Physics.py
+++ Physics.py
@@ -1,17 +1,29 @@
 import numpy as np
-import Forces as force
+from Forces import Forces 
 
 
 def doTheThing():
     print("howdy")
 
+    force1 = Forces()
+
     testMass = 23.0 #in kg
 
-    gForce = force.getGravitationalForce(testMass)
+    gForce = force1.getGravitationalForce(testMass)
 
     print('A ' + str(testMass) + ' kg person on earth experiences a gravitational force of ' + str(gForce) + ' m/s^2')
-    
 
+    force1.setGravity(10.0)
+    gForce = force1.getGravitationalForce(testMass)
+    print('A ' + str(testMass) + ' kg person on earth experiences a gravitational force of ' + str(gForce) + ' m/s^2')
+
+      
+    tension = 45.0 #in N
+
+    force1.setTensionForce(tension)
+    result = force1.getTensionForce()
+
+    print('The dog pulled with ' + str(result) + ' N of force')
 
 
 
