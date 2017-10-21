477aad3f0eceedff104e44055953e525946f0475
Josh Overbeck <overbejt@miamioh.edu>
2017-10-21 07:51:34 -0400

    connected Forces class to physics engine

--- Physics.py
+++ Physics.py
@@ -1,9 +1,16 @@
 import numpy as np
-
+import Forces as force
 
 
 def doTheThing():
     print("howdy")
+
+    testMass = 23.0 #in kg
+
+    gForce = force.getGravitationalForce(testMass)
+
+    print('A ' + str(testMass) + ' kg person on earth experiences a gravitational force of ' + str(gForce) + ' m/s^2')
+    
 
 
 
